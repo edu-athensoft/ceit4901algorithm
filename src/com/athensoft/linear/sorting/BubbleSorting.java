7edbdc824cd707236f2fb5f5185720aa101de5a9
Athensoft <inf.athensoft@hotmail.com>
2019-07-02 02:00:30 -0400

    added bubble sorting

---
+++ src/com/athensoft/linear/sorting/BubbleSorting.java
@@ -0,0 +1,36 @@
+package com.athensoft.linear.sorting;
+
+public class BubbleSorting {
+
+	public static void main(String[] args) {
+		int[] num = {4,5,2,7,8,9,1,6,3,0};
+		
+		//printArray(sort(num));
+		sort(num);
+		
+
+	}
+	
+	public static void printArray(int[] a) {
+		for(int x : a) {
+			System.out.print(x+"\t");
+		}
+		System.out.println();
+	}
+	
+	public static int[] sort(int[] a) {
+		int temp = 0;
+		for(int i=0; i<a.length-1; i++) {
+			for(int j=0; j<a.length-1-i; j++) {
+				if(a[j]>a[j+1]) {
+					temp = a[j];
+					a[j]=a[j+1];
+					a[j+1]=temp;
+				}
+			}
+			printArray(a);
+		}
+		return a;
+	}
+
+}
